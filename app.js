6a97e4e7928eba64fa96cf8357c272bebe73823c
Sharon <sharonvandergeest@live.nl>
2016-05-13 16:15:02 +0200

    made the user info app

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-
 var express = require('express')
 var bodyParser = require('body-parser')
 var fs = require('fs')
-var filereader = require('./filereader.js')
-
+var filereader = require('./src/filereader.js')
 var app = express()
-
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Part 0 - route 1 - show users
 
@@ -14,39 +12,86 @@ app.set('view engine', 'jade')
 
 app.get('/', function(req, res){
 
-	filereader.JSONreader('./users.json', callback)
+	filereader.JSONreader('./users.json', 
+		function (parsedJSON){
+			res.render('index', {names: parsedJSON})
+		} )
 
-	function callback(parsedJSON){
-		parsedJSON.forEach(function(key)){
-			names.push(key)
+} );
+
+
+// Part 1 - route 2 & 3 - search and show users on new page
+
+app.get('/search', function(req, res){ 
+
+	res.render('search')
+
+} );
+
+app.post('/result', function(req, res){
+
+	var storeUser = []
+	var usersearch = req.body.userinput
+	console.log("post")
+	filereader.JSONreader('./users.json', function (parsedJSON){
+		console.log('filereader werkt')
+		for( i=0; i<parsedJSON.length; i++ ){
+			if(usersearch == parsedJSON[i].firstname || usersearch == parsedJSON[i].lastname){
+				storeUser.push(usersearch)
+				var userFirst = parsedJSON[i].firstname
+				var userLast = parsedJSON[i].lastname
+				console.log('pushed')
+			}
+
 		}
+		if(storeUser.length > 0){
+			res.send("Name user: " + userFirst + " " + userLast)
+		} else {
+			res.send('No such user was found.')
+		}
+	} )
+
+} );
+
+
+
+// Part 2 - route 4 & 5 - Create user and send to users.json
+
+//simply render a page with app.get
+//make a jade page with three forms on there and a submit button
+//make a method post on the jade file to send the data back
+//make a new post route with app.post
+//inside make a function that sends the new user to users.json
+// How to make this function?
+
+app.get('/create', function(req, res){
+	res.render('create')
+})
+
+app.post('/add', function(req,res){
+
+	var createFirstname = req.body.createFirstname
+	var createLastname = req.body.createLastname
+	var createEmail = req.body.createEmail
+
+	if ( createFirstname.length > 0 && createLastname.length > 0 && createEmail.length > 0 ) {
+		filereader.JSONreader('./users.json', function (parsedJSON){
+			console.log('pasredjson ' + parsedJSON)
+			parsedJSON.push({firstname: createFirstname, lastname: createLastname, email: createEmail})
+			console.log('pushedjson ' + parsedJSON)
+			fs.writeFile('./users.json', JSON.stringify(parsedJSON), function() {
+				//res.send(parsedJSON)
+			})
+
+		} );
 	}
+	res.redirect('/')
 
-	res.render('index', {names: parsedData})
-});
-
-
-// Part 1 - route 2 - search users
-
-separateModule.JSONreader('./countries.json', callback);
-
-function callback(parsedJSON){
-	parsedJSON.forEach(function(key){
-		if (input === key.name){
-			console.log("Country: " + key.name);
-			console.log("Top Level Domain: " + key.topLevelDomain)
-		}
-	});
-}
-
-
-
-
-
+} )
 
 
 
 var server = app.listen(3000, function () {
-	console.log('Example app listening on port: ' + server.address().port);
+	console.log('User info app listening on port: ' + server.address().port);
 });
 
