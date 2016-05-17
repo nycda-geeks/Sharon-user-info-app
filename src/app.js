21f84f54f7b5e3b246ab7f9a4075dad0711dce8f
Sharon <sharonvandergeest@live.nl>
2016-05-17 11:54:03 +0200

    moved app.js to src folder and tested

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var fs = require('fs')
-var filereader = require('./src/filereader.js')
+var filereader = require('./filereader.js')
 var app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 
